ae88a40e690e20e5f4ebbfe0e1720b539fb80a8d
Sana Khan <ssk230031@utdallas.edu>
2025-12-01 15:24:00 -0600

    Implemented cowsay to make it more fun

--- src/photo_manipulation.py
+++ src/photo_manipulation.py
@@ -3,49 +3,54 @@ import glob
 import os
 import moviepy.editor as mpy
 from tqdm import tqdm
+import cowsay
 
 def main():
-    images = input("Welcome to the Python Photo Manipulation Editor! "
-        "Are you uploading an image or series of images? ").casefold()
+    images = cowsay_input("Welcome to the Python Photo Manipulation Editor! "
+        "Are you uploading an image or series of images? ")
 
     if images == 'image':
-        file_path_single = input("Please upload your image here! Include the full filepath with backslashes (case sensitive)! ")
+        file_path_single = cowsay_input("Please upload your image here! Include the full filepath with backslashes (case sensitive)! ")
         if os.path.exists(file_path_single):
-            type_single = input("What type of format would you like to convert your image to?\n"
+            type_single = cowsay_input("What type of format would you like to convert your image to?\n"
                 "PNG\nJPEG\nHEIC\n\n").casefold()
 
             if type_single in ('png', 'jpeg', 'jpg', 'heic'):
                 convert_image(file_path_single, output_format=type_single)
             else:
-                print("Sorry! That format is not accepted!")
+                cowsay.cow("Sorry! That format is not accepted!")
         else:
-            print("The specified file path does NOT exist!")
+            cowsay.cow("The specified file path does NOT exist!")
 
     elif images == 'images':
-        file_path_multiple = input("Please upload your images directory here! Include the full filepath with backslashes (case sensitive)! ")
+        file_path_multiple = cowsay_input("Please upload your images directory here! Include the full filepath with backslashes (case sensitive)! ")
         if os.path.exists(file_path_multiple):
-            type_multiple = input("What type of format would you like to convert your images to?\n"
-                "PNG\nJPEG\nHEIC\nGIF\nMOV\nMP4\n\n").casefold()
+            type_multiple = cowsay_input("What type of format would you like to convert your images to?\n"
+                "PNG\nJPEG\nHEIC\nGIF\nMP4\n\n").casefold()
 
             if type_multiple == 'gif':
                 output_gif_path = os.path.join(file_path_multiple, 'output.gif')
                 convert_images_to_gif(file_path_multiple, output_gif_path)
 
-            elif type_multiple in ('mov', 'mp4'):
+            elif type_multiple in ('mp4'):
                 output_video_path = os.path.join(file_path_multiple, f'output.{type_multiple}')
                 convert_images_to_video(file_path_multiple, output_video_path)
 
             elif type_multiple in ('png', 'jpeg', 'jpg', 'heic'):
-                print("Each image will be converted to specified format! :).")
+                cowsay.tux("Each image will be converted to specified format! :).")
                 convert_images_in_folder(file_path_multiple, type_multiple)
 
             else:
-                print("Sorry! That format is not accepted! Please try again with the available options!")
+                cowsay.cow("Sorry! That format is not accepted! Please try again with the available options!")
 
         else:
-            print("The specified file path does NOT exist! Please try again!")
+            cowsay.cow("The specified file path does NOT exist! Please try again!")
     else:
-        print("Sorry! That format is NOT accepted!")
+        cowsay.cow("Sorry! That format is NOT accepted!")
+
+def cowsay_input(message):
+    cowsay.kitty(message)
+    return input("> ").casefold()
 
 
 def convert_image(file_path, output_format):
@@ -54,9 +59,9 @@ def convert_image(file_path, output_format):
         base, _ = os.path.splitext(file_path)
         output_path = f"{base}.{output_format.lower()}"
         img.save(output_path, output_format.upper())
-        print(f"Image converted and saved as {output_path}")
+        cowsay.turtle(f"Image converted and saved as {output_path}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        cowsay.cow(f"An error occurred: {e}")
 
 
 def convert_images_to_gif(image_folder, output_path, duration=500):
@@ -64,7 +69,7 @@ def convert_images_to_gif(image_folder, output_path, duration=500):
     img_files.sort()
     images = [Image.open(img) for img in img_files]
     images[0].save(output_path,save_all=True,append_images=images[1:],duration=duration,loop=0)
-    print(f"GIF created and saved as {output_path}")
+    cowsay.turtle(f"GIF created and saved as {output_path}")
 
 
 def convert_images_to_video(image_folder, output_path, fps=24):
@@ -72,7 +77,7 @@ def convert_images_to_video(image_folder, output_path, fps=24):
     img_files.sort()
     clip = mpy.ImageSequenceClip(img_files, fps=fps)
     clip.write_videofile(output_path)
-    print(f"Video created and saved as {output_path}")
+    cowsay.turtle(f"Video created and saved as {output_path}")
 
 
 def convert_images_in_folder(image_folder, output_format):
@@ -80,7 +85,7 @@ def convert_images_in_folder(image_folder, output_format):
         img_files= glob.glob(os.path.join(image_folder, '*'))
         img_files.sort()
         if not img_files:
-            print("No images were found! Please try again!")
+            cowsay.cow("No images were found! Please try again!")
             return
         output_dir = os.path.join(image_folder, f"converted_{output_format.lower()}")
         os.makedirs(output_dir, exist_ok=True)
@@ -94,9 +99,9 @@ def convert_images_in_folder(image_folder, output_format):
                 converted_count += 1
             except Exception as e:
                 continue
-        print(f"Successfully converted {converted_count} of {len(img_files)} images to {output_format.upper()} format.")
+        cowsay.turtle(f"Successfully converted {converted_count} of {len(img_files)} images to {output_format.upper()} format.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        cowsay.cow(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
